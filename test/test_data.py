a82432207f6e76dee63f6b356df30fe784b5b54f
allenbo <linjianfengqrh@gmail.com>
2013-09-05 17:52:47 +0000

    Add test for cifar loader.

--- test/test_data.py
+++ test/test_data.py
@@ -1,15 +1,40 @@
-from striate import data, util
+from fastnet import data, util
+from fastnet.util import print_matrix
+import numpy as np
+import time
 
 def test_imagenet_loader():
-  df = data.ImageNetDataProvider('/ssd/nn-data/imagenet/', 
+  df = data.DataProvider.get_by_name('imagenet')(
+                                 '/ssd/nn-data/imagenet/', 
                                  batch_range=range(1000), 
-                                 category_range=range(20),
-                                 batch_size=512)
-  util.log('Index: %s', df.curr_batch_index)
-  util.log('%s', df._get_next_batch()['data'].shape)
-  util.log('Index: %s', df.curr_batch_index) 
-  util.log('%s', df._get_next_batch()['data'].shape)
-  util.log('Index: %s', df.curr_batch_index) 
+                                 batch_size=256)
+  
+  for i in range(32):
+    st = time.time()
+    batch = df.get_next_batch(128)
+    print time.time() - st
+    print batch.labels
+    print batch.data.shape
+    time.sleep(0.5)
+  print batch.labels
 
+
+def test_cifar_loader():
+  data_dir = '/ssd/nn-data/cifar-10.old/'
+  dp = data.DataProvider.get_by_name('cifar10')(data_dir, [1])
+  batch_size = 128
+  
+  data_list = []
+  for i in range(11000):
+    batch = dp.get_next_batch(batch_size)
+    batch = batch.data.get()
+    data_list.append(batch)
+
+    if batch.shape[1] != batch_size:
+      break
+  batch = np.concatenate(data_list, axis = 1)
+  print_matrix(batch, 'batch')
+  
 if __name__ == '__main__':
   test_imagenet_loader()
+  test_cifar_loader()
