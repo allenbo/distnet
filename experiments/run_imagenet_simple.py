ea450cc051eca86a0a23d862fa518dca0c242a6a
allenbo <linjianfengqrh@gmail.com>
2014-09-19 13:47:26 -0400

    Trival update on susitna

--- experiments/run_imagenet_simple.py
+++ experiments/run_imagenet_simple.py
@@ -13,7 +13,7 @@ from distbase import util
 
 test_id = 'imagenet-simple'
 
-checkpoint_dir = '/l0/checkpoint/'
+checkpoint_dir = '/ssd/checkpoint/'
 #checkpoint_dir = '/proj/FastNet/checkpoint/'
 param_file = './config/imagenet.cfg'
 output_dir = ''
@@ -22,9 +22,9 @@ output_method = 'disk'
 #data_dir = '/proj/FastNet/part-imagenet/'
 #train_range = range(3, 26) #1,2,3,....,40
 #test_range = range(1, 3) #41, 42, ..., 48
-data_dir = '/ssd/imagenet/'
-train_range = range(101, 1301) #1,2,3,....,40
-test_range = range(1, 101) #41, 42, ..., 48
+data_dir = '/ssd/part-imagenet-category/'
+train_range = range(11, 20) #1,2,3,....,40
+test_range = range(1, 10) #41, 42, ..., 48
 data_provider = 'imagenet'
 
 
@@ -44,7 +44,6 @@ test_freq = 100
 adjust_freq = 100
 factor = 1
 num_epoch = 30
-learning_rate = 0.1 * batch_size / 128
 image_color = 3
 image_shape = ConvDataLayout.get_output_shape(image_size, image_size, image_color, batch_size)
 net = parser.load_model(net.FastNet(image_shape), model)
